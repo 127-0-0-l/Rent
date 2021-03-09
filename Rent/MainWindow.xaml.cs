719698f8d3fd2f4abcab52074591bb14932b1014
Maksim <borisovets.maksim@gmail.com>
2021-03-09 22:53:19 +0300

    made authorization

--- Rent/MainWindow.xaml.cs
+++ Rent/MainWindow.xaml.cs
@@ -2,16 +2,19 @@
 using System.Windows;
 using Newtonsoft.Json;
 using System.IO;
+using System;
+using System.Linq;
+using System.Windows.Media;
 
 namespace Rent
 {
-    enum UserType
+    public enum UserType
     {
         LandLord,
         Renter
     }
 
-    enum PremisesType
+    public enum PremisesType
     {
         Room,
         Apartment
@@ -19,9 +22,70 @@ namespace Rent
 
     public partial class MainWindow : Window
     {
+        private List<User> users;
+        public User CurrentUser { get; private set; }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadUsers();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if(tbLogin.Text.Length == 0)
+                {
+                    throw new Exception("введите логин");
+                }
+
+                if (tbPassword.Text.Length == 0)
+                {
+                    throw new Exception("введите пароль");
+                }
+
+                if (users.Where(u => u.Login == tbLogin.Text).Count() == 0)
+                {
+                    throw new Exception("пользователя не существует");
+                }
+                else
+                {
+                    var user = users.Where(u => u.Login == tbLogin.Text).FirstOrDefault();
+
+                    if(user.Password != tbPassword.Text)
+                    {
+                        throw new Exception("пароль неверный");
+                    }
+                    else
+                    {
+                        CurrentUser = user;
+                        grdAuthorization.Visibility = Visibility.Hidden;
+                        grdMenu.Visibility = Visibility.Visible;
+
+                        if(CurrentUser.TypeOfUser == (int)UserType.LandLord)
+                        {
+                            btnCreateRecord.Visibility = Visibility.Hidden;
+                        }
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void LoadUsers()
+        {
+            using(StreamReader file = new StreamReader(@".\Resources\Users.txt"))
+            {
+                users = JsonConvert.DeserializeObject<List<User>>(file.ReadToEnd());
+            }
         }
     }
 }
--- Rent/User.cs
+++ Rent/User.cs
@@ -1,16 +1,20 @@
 ﻿namespace Rent
 {
-    class User
+    public class User
     {
         public UserType TypeOfUser { get; private set; }
         public string Name { get; private set; }
         public string PhoneNumber { get; private set; }
+        public string Login { get; private set; }
+        public string Password { get; private set; }
 
-        public User(UserType userType, string name, string phoneNumber)
+        public User(UserType userType, string name, string phoneNumber, string login, string password)
         {
             TypeOfUser = userType;
             Name = name;
             PhoneNumber = phoneNumber;
+            Login = login;
+            Password = password;
         }
     }
 }
